f68036e482cbb4332579b76c21466903d81c098f
Mahmoud Barzegar <m.bardehgar.hedeshi@gmail.com>
2022-10-10 10:20:54 +0330

    :up: (hello): Refactor code

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,5 +1,2 @@
-<?php
 
-$name = $request->query->get('name', 'World');
-?>
-Hello <?= htmlspecialchars(ucfirst($name), ENT_QUOTES, 'UTF-8') ?>
+Hello <?= htmlspecialchars(isset($name) ? ucfirst($name) : 'World', ENT_QUOTES, 'UTF-8') ?>
